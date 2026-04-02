ba0dd3f58cd93eb64c21162d9ed937c8a58bed70
cross-mind <2249688409@qq.com>
2026-04-02 15:48:09 +0100

    first commit

--- frontend-demo/src/lib/campusOnboarding.ts
+++
@@ -1,9 +0,0 @@
-const KEY = 'szu_memoir_campus_chosen'
-
-export function hasCompletedCampusOnboarding(): boolean {
-  return localStorage.getItem(KEY) === '1'
-}
-
-export function markCampusOnboardingDone(): void {
-  localStorage.setItem(KEY, '1')
-}
